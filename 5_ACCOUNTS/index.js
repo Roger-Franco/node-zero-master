8e83943620fc1be7e9482032c0f2ad9119baf225
Roger-Franco <rogerfrancogalt@gmail.com>
2023-01-05 09:06:28 -0400

    Consulta de saldo implementada

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -22,7 +22,7 @@ function operation() {
     } else if (action === 'Depositar') {
       deposit()
     } else if (action === 'Consultar Saldo') {
-
+      getAccountBalance()
     } else if (action === 'Sacar') {
 
     } else if (action === 'Sair') {
@@ -138,4 +138,26 @@ function getAccount(accountName) {
   })
 
   return JSON.parse(accountJSON)
+}
+
+function getAccountBalance() {
+  inquirer.prompt([
+    {
+      name: 'accountName',
+      message: 'Qual o nome da sua conta?'
+    }
+  ]).then((answer) => {
+    const accountName = answer['accountName']
+
+    if (!checkAccount(accountName)) {
+      return getAccountBalance()
+    }
+
+    const accountBalance = getAccount(accountName)
+
+    console.log(chalk.bgBlue.black(`Olá, o saldo da sua conta é de R$${accountBalance.balance}`))
+
+    operation()
+
+  }).catch(err => console.log(err))
 }
