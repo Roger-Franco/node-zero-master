34f8e3b71fe289abe2b679c46fb0d278838b96d0
Roger Franco <rogerfrancogalt@gmail.com>
2023-01-27 21:41:45 -0400

    Regatando apenas um dado

---
+++ 12_MONGODB/5_resgatando_dado/controllers/ProductController.js
@@ -0,0 +1,32 @@
+const Product = require('../models/Product')
+
+module.exports = class ProductController {
+  static async showProducts(req, res) {
+    const products = await Product.getProducts()
+    res.render('products/all', {products})
+  }
+
+  static createProduct(req, res) {
+    res.render('products/create')
+  }
+
+  static createProductPost(req, res) {
+    const {name, image, price, description} = req.body
+
+    const product = new Product(name, image, price, description)
+
+    product.save()
+
+    res.redirect('/products')
+  }
+
+  static async getProduct(req, res) {
+    const id = req.params.id
+
+    const product = await Product.getProductById(id)
+
+    console.log(product)
+
+    res.render('products/product', {product})
+  }
+}
