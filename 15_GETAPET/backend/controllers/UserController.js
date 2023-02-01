9c99f1f1c12a9bea3ad59bc304e9503322af4041
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-01 12:45:01 -0400

    Registrando usuário no sistema

--- 15_GETAPET/backend/controllers/UserController.js
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -1,7 +1,69 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/User')
 
 module.exports = class UserController {
   static async register(req, res) {
-    res.json('Olá Get a Pet!')
+    const {name, email, phone, password, confirmpassword} = req.body
+
+    // Validations
+    if (!name) {
+      res.status(422).json({message: 'O nome é obrigatório!'})
+      return
+    }
+    
+    if (!email) {
+      res.status(422).json({message: 'O e-mail é obrigatório!'})
+      return
+    }
+
+    if (!phone) {
+      res.status(422).json({message: 'O telefone é obrigatório!'})
+      return
+    }
+
+    if (!password) {
+      res.status(422).json({message: 'A senha é obrigatório!'})
+      return
+    }
+
+    if (!confirmpassword) {
+      res.status(422).json({message: 'A confirmação de senha é obrigatório!'})
+      return
+    }
+
+    if (password !== confirmpassword) {
+      res.status(422).json({message: 'A senha e a confirmação de senha precisam ser iguais!'})
+      return
+    }
+
+    // check if user exists
+    const userExists= await User.findOne({email: email})
+    if (userExists) {
+      res.status(422).json({message: 'Por favor, utilize outro e-mail!'})
+      return
+    }
+
+    // create a password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create a user
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    })
+
+    try {
+      const newUser = await user.save()
+      res.status(201).json({message: 'Usuário criado!', newUser})
+    } catch (error) {
+      res.status(500).json({message: error})
+    }
+
+    // res.json({message: 'Deu certo!!'})
   }
 }
