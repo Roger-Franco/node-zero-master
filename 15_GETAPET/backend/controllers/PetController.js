2d963cf0f1930431cd03e773c1a52a201e308451
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-04 13:01:58 -0400

    Resgatando todas as adoções

--- 15_GETAPET/backend/controllers/PetController.js
+++ 15_GETAPET/backend/controllers/PetController.js
@@ -84,4 +84,13 @@ module.exports = class PetController {
      const pets = await Pet.find({'user._id': user._id}).sort('-createdAt')
      res.status(200).json({pets})
   }
+
+  static async getAllUserAdoptions(req, res) {
+    // get user from token
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+     const pets = await Pet.find({'adopter._id': user._id}).sort('-createdAt')
+     res.status(200).json({pets})
+  }
 }
