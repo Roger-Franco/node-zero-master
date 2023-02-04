2635e6fe10ae2ab9af5b357c552ff7db35d60b1c
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-04 15:26:43 -0400

    Removendo Pet do sistema

--- 15_GETAPET/backend/controllers/PetController.js
+++ 15_GETAPET/backend/controllers/PetController.js
@@ -98,6 +98,7 @@ module.exports = class PetController {
   static async getPetById(req, res) {
     const id = req.params.id
 
+    // check if id is valid
     if(!ObjectId.isValid(id)) {
       res.status(422).json({message: 'ID inválido!'})
       return
@@ -109,5 +110,33 @@ module.exports = class PetController {
       res.status(404).json({message: 'Pet não ecnontrado!'})
     }
     res.status(200).json({pet:pet})
+  }
+
+  static async removePetById(req, res) {
+    const id = req.params.id
+
+    // check if id is valid
+    if(!ObjectId.isValid(id)) {
+      res.status(422).json({message: 'ID inválido!'})
+      return
+    }
+    // check if pet exists
+    const pet = await Pet.findOne({_id: id})
+
+    if(!pet) {
+      res.status(404).json({message: 'Pet não ecnontrado!'})
+    }
+
+    // check if logged in user registered the pet
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    if(pet.user._id.toString() !== user._id.toString()) {
+      res.status(404).json({message: 'Houve um problema em procesar a sua solicitação, tente novamente mais tarde!'})
+    }
+
+    await Pet.findByIdAndRemove(id)
+
+    res.status(200).json({message:'Pet removido com sucesso!'})
   }
 }
