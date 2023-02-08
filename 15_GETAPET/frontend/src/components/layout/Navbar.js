06f1babd3d7e0c5fa78268263d3ccc3179af6519
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-07 21:41:54 -0400

    Persistindo autenticação no front

--- 15_GETAPET/frontend/src/components/layout/Navbar.js
+++ 15_GETAPET/frontend/src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import styles from './Navbar.module.css'
 
 import Logo from '../../assets/img/logo.png'
 
+/* context */
+import { Context } from '../../context/UserContext'
+
 export default function Navbar() {
+  const {authenticated} = useContext(Context)
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
@@ -16,12 +20,20 @@ export default function Navbar() {
         <li>
         <Link to="/">Adotar</Link>
         </li>
-        <li>
-        <Link to="/login">Entrar</Link>
-        </li>
-        <li>
-        <Link to="/register">Cadastrar</Link>
-        </li>
+        {authenticated ? (
+        <>
+          <p>Logado</p>
+        </>
+        ) : (
+          <>
+          <li>
+            <Link to="/login">Entrar</Link>
+          </li>
+          <li>
+            <Link to="/register">Cadastrar</Link>
+          </li>
+        </>
+        )}
       </ul>
     </nav>
   )
