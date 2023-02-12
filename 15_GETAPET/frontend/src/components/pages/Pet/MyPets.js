6206e9185ce125892b2cd823387d559a17a02177
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-12 16:52:12 -0400

    Exibindo os pets na Deshboard

--- 15_GETAPET/frontend/src/components/pages/Pet/MyPets.js
+++ 15_GETAPET/frontend/src/components/pages/Pet/MyPets.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import styles from './Dashboard.module.css'
+
 /*  hooks */
 import useFlashMessage from '../../../hooks/useFlashMessage'
 import api from '../../../utils/api'
+import RoundedImage from '../../layout/RoundedImage'
 
 export default function MyPets() {
   const [pets, setPets] = useState([])
@@ -27,7 +30,29 @@ export default function MyPets() {
       <Link to='/pet/add'>Cadastrar Pet</Link>
       </div>
     <div>
-      {pets.length > 0 && <p>Meus Pets cadastrados</p>}
+      {pets.length > 0 && 
+      pets.map((pet) => (
+        <div key={pet.id}>
+          <RoundedImage
+            src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+            alt={pet.name}
+            width='75px'
+          />
+          <span className="bold">{pet.name}</span>
+          <div className={styles.actions}>
+            {pet.available ? (
+              <>
+              {pet.adopter && <button>Concluir adoção</button>}
+              <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+              <button>Excluir</button>
+              </>
+            ) : (
+              <p>Pet já adotado</p>
+            )}
+          </div>
+        </div>
+      ))
+      }
       {pets.length == 0 && <p>Não há Pets cadastrados</p>}
     </div>
     </section>
