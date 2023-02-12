1b3242cace610c5e97455352d9722524bc9d866c
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-12 16:33:07 -0400

    Requisição para pegar pets cadastrados

--- 15_GETAPET/frontend/src/components/pages/Pet/MyPets.js
+++ 15_GETAPET/frontend/src/components/pages/Pet/MyPets.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+
+/*  hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+import api from '../../../utils/api'
 
 export default function MyPets() {
   const [pets, setPets] = useState([])
+  const [token] = useState(localStorage.getItem('token') || '')
+  const {setFlashMessage} = useFlashMessage()
+
+  useEffect(() => {
+    api.get('pets/mypets', {
+      headers: {
+      Authorization: `Bearer ${JSON.parse(token)}`,
+      }
+    }).then((response) => {
+      setPets(response.data.pets)
+    })
+  }, [token])
+  
   return (
     <section>
       <div>
