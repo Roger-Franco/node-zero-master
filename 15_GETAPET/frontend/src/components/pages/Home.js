e6d47e5e6d653b6ff09553a95b0135d936bfe96f
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-13 12:25:09 -0400

    Resgatando os pets na Home

--- 15_GETAPET/frontend/src/components/pages/Home.js
+++ 15_GETAPET/frontend/src/components/pages/Home.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../utils/api'
+
+import styles from './Home.module.css'
 
 export default function Home() {
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    api.get('/pets').then((response) => {
+      setPets(response.data.pets)
+      // console.log(pets);
+      // console.log(response);
+    })
+    
+  }, [])
+  // console.log(pets);
+
   return (
     <section>
       <h1>Home</h1>
