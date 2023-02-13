8462f3e8972eb2a945583f01d4b0d75e22f8b5fb
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-12 21:45:09 -0400

    Css da dashboard

--- 15_GETAPET/frontend/src/components/layout/RoundedImage.js
+++ 15_GETAPET/frontend/src/components/layout/RoundedImage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import styles from './RoundedImage.module.css'
 
-export default function RoundedImage({width, src, alt}) {
+export default function RoundedImage({src, alt, width}) {
   return (
     <img
     className={`${styles.rounded_image} ${styles[width]}`}
