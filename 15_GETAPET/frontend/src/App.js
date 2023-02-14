36161d0b5bc12d65fb0a5f5bd8a69673abf28909
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-14 12:30:50 -0400

    Pagina de detalhes do Pet

--- 15_GETAPET/frontend/src/App.js
+++ 15_GETAPET/frontend/src/App.js
@@ -23,6 +23,7 @@ import {UserProvider} from './context/UserContext'
 import MyPets from './components/pages/Pet/MyPets';
 import AddPet from './components/pages/Pet/AddPet';
 import EditPet from './components/pages/Pet/EditPet';
+import PetDetails from './components/pages/Pet/PetDetails';
 
 function App() {
   return (
@@ -38,6 +39,7 @@ function App() {
               <Route path="/pet/mypets" element={<MyPets />} />
               <Route path="/pet/add" element={<AddPet />} />
               <Route path="/pet/edit/:id" element={<EditPet />} />
+              <Route path="/pet/:id" element={<PetDetails />} />
               <Route path="/" element={<Home />} />
             </Routes>
           </Container>
