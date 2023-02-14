14011de62188073e852062c62c3a168a81241358
Roger Franco <rogerfrancogalt@gmail.com>
2023-02-13 23:15:55 -0400

    Loop para exibir pets na Home

--- 15_GETAPET/frontend/src/components/pages/Home.js
+++ 15_GETAPET/frontend/src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import api from '../../utils/api'
 
 import styles from './Home.module.css'
@@ -18,7 +19,28 @@ export default function Home() {
 
   return (
     <section>
-      <h1>Home</h1>
+      <div>
+        <h1>Adote um Pet</h1>
+        <p>Veja os detalhes de cada um e conheça o tutor deles</p>
+      </div>
+      <div>
+        {pets.length > 0 &&
+        pets.map((pet) => (
+          <div>
+            <p>Imagem do Pet</p>
+            <h3>{pet.name}</h3>
+            <p>
+              <span className="bold">Peso:</span> {pet.weight}kg
+            </p>
+            {pet.available ? (
+              <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
+            ) : (
+              <p>Adotado</p>
+            ) }
+          </div>
+        ))}
+        {pets.length === 0 && <p>Não há pets cadastrados ou disponíveis para adoção no momento</p> }
+      </div>
     </section>
   )
 }
